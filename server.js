13fe4025a0dd5e1a0c4a6229262dfd79878d206a
valentine896 <amunonye@yahoo.com>
2023-06-01 19:24:40 -0500

    Initial commit

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const axios = require('axios');
 
 const app = express();
 
-// Allow your extension's origin
-app.use(cors({
-  origin: 'chrome-extension://ifopojflepcoiddjblkegphdpolcaoac'
-}));
-
+app.use(cors());
 app.use(bodyParser.json());
 
 const apiKey = process.env.SUMMARIZER_API_KEY;
@@ -17,20 +13,33 @@ const apiKey = process.env.SUMMARIZER_API_KEY;
 const calculateMaxTokens = (inputText) => {
   const inputTokens = inputText.split(' ').length;
   const estimatedSummaryTokens = Math.ceil(inputTokens * 0.2);
-  return Math.min(estimatedSummaryTokens, 200);
+  return Math.min(estimatedSummaryTokens, 6000);
 };
 
 app.post('/summarize', async (req, res) => {
+  console.log('Received request to /summarize');
   const { text } = req.body;
+  console.log('Text to summarize:', text);
   const maxTokens = calculateMaxTokens(text);
 
   try {
+    console.log('Sending request to GPT-3 API...');
     const gpt3Response = await axios.post(
-      'https://api.openai.com/v1/engines/davinci-codex/completions',
+      'https://api.openai.com/v1/chat/completions',
       {
-        prompt: `${text}\n\nCan you summarize the above post in a few sentences?`,
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: text,
+          },
+          {
+            role: "system",
+            content: "Please provide a 5-7 sentence summary of the above text that captures the main idea and conclusion in first person.",
+          },
+        ],
         max_tokens: maxTokens,
-        temperature: 0.3,
+        temperature: 0,
       },
       {
         headers: {
@@ -40,10 +49,13 @@ app.post('/summarize', async (req, res) => {
       }
     );
 
+    console.log('Received response from GPT-3 API');
+
     if (gpt3Response.data.choices && gpt3Response.data.choices.length > 0) {
-      const summary = gpt3Response.data.choices[0].text.trim();
+      const summary = gpt3Response.data.choices[0].message.content.trim();
       console.log('GPT-3 API response:', gpt3Response.data);
       console.log('Generated summary:', summary);
+      res.header('Access-Control-Allow-Origin', '*'); // Add this line to set the CORS header
       res.json({ summary });
     } else {
       console.log('No choices found in GPT-3 API response:', gpt3Response.data);
@@ -54,6 +66,7 @@ app.post('/summarize', async (req, res) => {
     res.status(500).json({ error: `Error calling GPT-3 API: ${error.message}` });
   }
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
